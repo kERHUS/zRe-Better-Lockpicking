54c26442d7a4711f66ddd4eca950e38b79a70c77
kERHUS <67560865+kERHUS@users.noreply.github.com>
2023-09-27 19:14:22 +0500

    Update zReBL_commands.lua

--- lua/server/zReBL_commands.lua
+++ lua/server/zReBL_commands.lua
@@ -1,7 +1,7 @@
+-- (c) Chuckleberry "GOD OF GODS" Finn
+
 ---THIS IS CALLED IN CLIENT
 --sendClientCommand(self.character, 'crowbar', 'vehicleDoor', { vehicle=lockpick_object:getVehicle():getId(), part=lockpick_object:getId() })
-
----THIS GOES IN /SERVER/
 local function crowbarCommands(module, command, player, args)
 
     if module == "crowbar" and command == "vehicleDoor" then
@@ -14,10 +14,32 @@ local function crowbarCommands(module, command, player, args)
         if not part:getDoor() then return end
 
         part:getDoor():setLocked(false)
-        part:getDoor():setOpen(true)
-        part:getDoor():setLockBroken(true)
+		part:getDoor():setOpen(true)
+        --part:getDoor():setLockBroken(true)
 
         vehicle:transmitPartDoor(part)
     end
 end
 Events.OnClientCommand.Add(crowbarCommands)
+
+
+
+---THIS IS CALLED IN CLIENT
+--sendClientCommand(self.character, 'screwdriver', 'vehicleDoor', { vehicle=lockpick_object:getVehicle():getId(), part=lockpick_object:getId() })
+local function screwdriverCommands(module, command, player, args)
+
+    if module == "screwdriver" and command == "vehicleDoor" then
+        local vehicle = getVehicleById(args.vehicle)
+        if not vehicle then return end
+
+        local part = vehicle:getPartById(args.part)
+        if not part then return end
+
+        if not part:getDoor() then return end
+
+        part:getDoor():setLocked(false)
+
+        vehicle:transmitPartDoor(part)
+    end
+end
+Events.OnClientCommand.Add(screwdriverCommands)
